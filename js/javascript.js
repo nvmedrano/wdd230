662e6ec3a66ca3628883badbafdc03ce2b3dc2cb
nvmedrano <noemed7@gmail.com>
2021-09-23 13:32:15 -0700

    Portal Updates

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+
+var currentYear = new Date().getFullYear();
+var footyear = document.querySelector(".curryear");
+footyear.textContent = currentYear; 
+
+var lastModDate = document.lastModified
+document.getElementById("lastupdate").textContent = lastModDate;
